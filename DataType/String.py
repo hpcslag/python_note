a577b23211c697c39b3ab7f062e2ca91e8ef617a
MacTaylor <cslag@hotmail.com.tw>
2015-03-19 21:32:02 +0800

    Create String.py

---
+++ DataType/String.py
@@ -0,0 +1,21 @@
+"Hello world"[1] //H
+
+"Hello world"[1:3] //Hel
+
+"{0}".format("Hello world")//Hello world
+
+number = 10
+string = "Hello world"
+
+string + number //Error
+print(string,number) //Hello world10
+string + str(number) //Hello world10
+
+len(string) //11
+
+for s in string:
+  print(s+"1")
+  //H1l1l1o1 1w1o1r1l1d1
+
+int("12") //12
+
