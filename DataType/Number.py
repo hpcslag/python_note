a16737b5f6df01d3efa5a6372a97621c8bdd1550
MacTaylor <cslag@hotmail.com.tw>
2015-03-19 21:40:38 +0800

    Create Number.py

---
+++ DataType/Number.py
@@ -0,0 +1,15 @@
+import math
+
+math.pow(3,2) //9.0
+
+math.pi //3.141592653589793
+
+math.sqrt(9) //3.0
+
+/*Complex*/
+z = -89.12345+2.124j
+z.real,z.imag //(-89.123,2.125)
+
+/*Decimal*/
+import decimal
+x = decimal.Decimal(16167) //Decimal(16167)
