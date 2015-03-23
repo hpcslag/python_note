4b27ca55231308f9ab7d5b1fd664a0e61b4e1b0e
MacTaylor <cslag@hotmail.com.tw>
2015-03-23 14:44:51 +0800

    Update WriteFile.py

--- IO/File/WriteFile.py
+++ IO/File/WriteFile.py
@@ -1,7 +1,7 @@
 s = "Hello world this file created by python3 file module"
 
 try:
-  FILE = open('Test.txt')
+  FILE = open('Test.txt','w')#write file pattrn
   FILE.write("{0}".format(s))
   FILE.close()
   print("Write file -> Test.txt ")
